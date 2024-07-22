f45bf0f8f25ac93f93f7af502bff4c7c43dd4dc7
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-07-22 14:02:39 +0530

    Add static adapter

--- packages/site/svelte.config.js
+++ packages/site/svelte.config.js
@@ -1,10 +1,10 @@
-import {adapterNetlifyEdgeFunctions} from '@hrishikeshk/sveltekit-adapter-netlify'
+import {adapterNetlifyStatic} from '@hrishikeshk/sveltekit-adapter-netlify'
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapterNetlifyEdgeFunctions({}),
+    adapter: adapterNetlifyStatic({}),
     alias: {
       '~/*': 'src/*'
     }
