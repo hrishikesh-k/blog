d46e095761001f031925214aadb41d22997d9e7e
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-08-01 15:13:44 +0530

    Fix blob path

--- packages/site/netlify/edge-functions/image.ts
+++ packages/site/netlify/edge-functions/image.ts
@@ -4,7 +4,7 @@ import wretch from 'https://esm.sh/wretch@2.9.0'
 export default async function (req : Request, context : Context) {
   const url = new URL(req.url)
   const segments = url.pathname.split('/')
-  const image = await wretch(`https://api.netlify.com/api/v1/blobs/${context.site.id}/images/${segments[2]}/${segments[3]}`).auth(`Bearer ${Netlify.env.get('NETLIFY_API_KEY')}`).get().blob()
+  const image = await wretch(`https://api.netlify.com/api/v1/blobs/${context.site.id}/img/${segments[2]}/${segments[3]}`).auth(`Bearer ${Netlify.env.get('NETLIFY_API_KEY')}`).get().blob()
   return new Response(image, {
     headers: {
       'cache-control': 'immutable, public, max-age=31536000',
